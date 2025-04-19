5269baf22a871d32bf158580684e3245bdf3cd13
Lesia-ya <lesialaba@gmail.com>
2025-04-19 18:43:59 +0300

    Update test_api.py

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -12,9 +12,9 @@ def test_remove_name():
     assert user.name == ''
 
 
-def test_name(user):
+def test_name():
     assert user.name == 'Sergii'
 
 
-def test_second_name(user):
+def test_second_name():
     assert user.second_name == 'Butenko'
