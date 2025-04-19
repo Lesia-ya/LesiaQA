f75ef5853bbc609638ab21bb25a521a240dfea0a
Lesia-ya <lesialaba@gmail.com>
2025-04-19 18:10:50 +0300

    Update test_api.py

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,8 +1,20 @@
-def test_check_math():
-    assert 7 * 7 == 49
+class User:
 
-def test_check_78():
-    assert 7 * 8 == 56
+    def __init__(self) -> None:
+        self.name = "Sergii"
+        self.second_name = "Butenko"
 
-def test_check_97():
-    assert 9 * 7 == 63
+
+user = User()
+
+def test_remove_name():
+    user.name = ''
+    assert user.name == ''
+
+
+def test_name(user):
+    assert user.name == 'Sergii'
+
+
+def test_second_name(user):
+    assert user.second_name == 'Butenko'
