e742b4fe3ec2470c4f3758ac35a84c23c194e2f1
Lesia-ya <lesialaba@gmail.com>
2025-04-26 22:01:46 +0300

    Create pytest_gitpull.py

---
+++ pytest_gitpull.py
@@ -0,0 +1,11 @@
+import subprocess
+import pytest
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config):
+    # Цей hook працює для будь-якого виклику pytest, навіть із параметрами
+    print("Виконується git pull перед тестами...")
+    result = subprocess.run(["git", "pull"], capture_output=True, text=True)
+    print(result.stdout)
+    if result.returncode != 0:
+        print("Помилка при виконанні git pull:", result.stderr)
