84dc9b3dfa1595f552e08af7d9745e2d885a33da
Lesia-ya <lesialaba@gmail.com>
2025-05-11 13:46:31 +0300

    Update test_github_api.py

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -4,6 +4,6 @@ from modules.api.clients.github import github
 
 @pytest.mark.api
 def test_user_exists():
-    api = github()
+    api = GitHub()
     user = api.get_user_defunkt()
     assert user['login'] == 'defunkt'
