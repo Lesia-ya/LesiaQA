e9ba6c1681d4cc096bad9f771265ed7e9c2793e8
Lesia-ya <lesialaba@gmail.com>
2025-05-11 14:29:14 +0300

    Update github.py

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -4,13 +4,7 @@ import requests
 class GitHub:
     
     def get_user(self, username):
-        r = requests.get(f'https://api.github.com/users/defunkt')
+        r = requests.get(f'https://api.github.com/users/{username}')
         body = r.json()
 
         return body
-
-    def get_non_exist_user(self):
-        r = requests.get(f'https://api.github.com/users/butenkosergii')
-        body = r.json()
-        
-        return body 
