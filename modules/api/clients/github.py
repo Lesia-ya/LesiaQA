3f8ab2543fc12d348bca6deed6737d665f44e8e7
Lesia-ya <lesialaba@gmail.com>
2025-05-11 14:16:15 +0300

    Update github.py

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -8,3 +8,9 @@ class GitHub:
         body = r.json()
 
         return body
+
+    def get_non_exist_user(self)
+        r = request.get(f'https://api.github.com/users/butenkosergii')
+        body = r.json()
+        
+        return body 
