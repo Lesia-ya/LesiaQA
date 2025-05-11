55772958bb76c2a6a790c87b96bd357bbde4f95a
Lesia-ya <lesialaba@gmail.com>
2025-05-11 14:41:43 +0300

    Update test_github_api.py

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -1,15 +1,12 @@
 import pytest
-from modules.api.clients.github import GitHub
 
 
 @pytest.mark.api
-def test_user_exists():
-    api = GitHub()
-    user = api.get_user('defunkt')
+def test_user_exists(github_api):
+    user = get_user('defunkt')
     assert user['login'] == 'defunkt'
 
 @pytest.mark.api
-def test_user_not_exists():
-    api = GitHub()
-    r = api.get_user('butenkosergii')
+def test_user_not_exists(github_api):
+    r = get_user('butenkosergii')
     assert r['message'] == 'Not Found'
