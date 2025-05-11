44d70381c907d888a0977166e19a6340916d63ca
Lesia-ya <lesialaba@gmail.com>
2025-05-11 13:54:52 +0300

    Update github.py

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -3,8 +3,8 @@ import requests
 
 class GitHub:
     
-    def get_user(self, username):
-        r = requests.get(f"https://api.github.com/users/defunkt")
+    def get_user(self):
+        r = requests.get(f'https://api.github.com/users/defunkt')
         body = r.json()
 
         return body
