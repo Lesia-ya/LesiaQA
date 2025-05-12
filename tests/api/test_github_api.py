006c5d09f9af83026a9ed86f5081773c2a8bdbbc
Lesia-ya <lesialaba@gmail.com>
2025-05-12 22:56:31 +0300

    Update test_github_api.py

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -16,6 +16,5 @@ def test_user_not_exists(github_api):
 @pytest.mark.api
 def test_repo_can_be_found(github_api):
     r = github_api.search_repo('become-qa-auto')
-    print(r)
-   # assert r['total_count'] == 57
-   # assert 'become-qa-auto' in r['items'][0]['name'] 
+    assert r['total_count'] == 57
+    assert 'become-qa-auto' in r['items'][0]['name'] 
