98b6ebe61fdc6df0ec34e8ad61266054e461f557
Lesia-ya <lesialaba@gmail.com>
2025-05-18 15:17:54 +0300

    Update database.py

--- modules/common/database.py
+++ modules/common/database.py
@@ -18,3 +18,10 @@ class Database():
         self.cursor.execute(query)
         record = self.cursor.fetchall()
         return record
+
+    def get_user_address_by_name(self, name):
+        query = f"SELECT address, city, postalCode, country FROM customers WHERE name = '{name}'"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+
