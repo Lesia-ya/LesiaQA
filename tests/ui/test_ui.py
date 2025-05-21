4a547f9bf9aeb074548d40dbfbe18ad134df1a2e
Lesia-ya <lesialaba@gmail.com>
2025-05-21 22:22:49 +0300

    Update test_ui.py

---
+++ tests/ui/test_ui.py
@@ -0,0 +1,17 @@
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+@pytest.mark.ui
+def test_check_incorrect_username():
+    # Створення об'єкту для керування бразуером
+    driver = webdriver.Chrome(
+        service=Service(r"/home/sbutenko/repos/LnD/Become QA Auto/" + "chromedriver")
+        )
+
+    # відкриваємо сторінку https://github.com/login 
+    driver.get("https://github.com/login")
+
+        # Закриваємо браузер
+    driver.close()
