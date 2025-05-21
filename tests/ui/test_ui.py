f012e0970b83591c766088ef2e83618fc8f70948
Lesia-ya <lesialaba@gmail.com>
2025-05-21 22:37:12 +0300

    Update test_ui.py

--- tests/ui/test_ui.py
+++ tests/ui/test_ui.py
@@ -2,13 +2,13 @@ import pytest
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.mark.ui
 def test_check_incorrect_username():
     # Створення об'єкту для керування бразуером
     driver = webdriver.Chrome(
-        service=Service(r"/home/sbutenko/repos/LnD/Become QA Auto/" + "chromedriver")
-        )
+        service=Service(ChromeDriverManager().install()))
 
     # відкриваємо сторінку https://github.com/login 
     driver.get("https://github.com/login")
